8f960ca1de9a0ef37b754c3cd81b54ec4969ff3e
Daniel Smilansky <dsmilansky@gmail.com>
2013-07-01 15:54:52 -0700

    Video Validations

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -11,4 +11,12 @@ describe Video do
     video = Video.reflect_on_association(:category)
     video.macro.should == :belongs_to
   end
+
+  it "should require a title" do
+    Video.new(:title => "").should_not be_valid
+  end
+
+  it "should require a description" do
+    Video.new(:description => "").should_not be_valid
+  end
 end
